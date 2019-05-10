f918a55bd388a104c3e3fd2938e579fbe6f4c361
luozhihui_vendor <luozhihui_vendor@sensetime.com>
2019-05-10 10:04:06 +0800

    部分修改

--- provider1/src/main/java/com/lzh/provider1/Provider1Application.java
+++ provider1/src/main/java/com/lzh/provider1/Provider1Application.java
@@ -1,18 +1,21 @@
 package com.lzh.provider1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
 @ComponentScan("com.lzh")
-@RefreshScope
+//@RefreshScope
 public class Provider1Application {
-
+	public static Logger logger = LoggerFactory.getLogger(Provider1Application.class);
 	public static void main(String[] args) {
+		logger.info("开始启动系统");
 		SpringApplication.run(Provider1Application.class, args);
+		logger.error("启动成功");
 	}
 }
